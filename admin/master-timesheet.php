3c8a3c31ee874346d73fa956f957fa4353c3d673
JOHN RYAN <john.ryan@rutgers.edu>
2014-02-06 22:35:12 -0500

    adjusting

--- admin/master-timesheet.php
+++ admin/master-timesheet.php
@@ -11,6 +11,10 @@ ob_end_clean();
 
 foreach($allUsers as $nextUser) {
 
+	foreach($_POST as $key=>$value)
+	{
+		echo preg_replace("$key";
+	}
 	if ($nextUser !== '') {
 		echo '<img src="timesheet2.php?startdate='.$_POST["startdate"]."&username=".$nextUser.'">';
 	}
