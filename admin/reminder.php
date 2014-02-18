2334412d521deba1fad2c4b51ca9eaaa80cf0ad6
JOHN RYAN <john.ryan@rutgers.edu>
2014-02-18 14:39:08 -0500

    progress

--- admin/reminder.php
+++ admin/reminder.php
@@ -5,13 +5,16 @@ echo "testing";
 include('../payroll.inc');
 
 $queryText = "SELECT * FROM `payrollinfo` WHERE `iscomplete`=0 AND `date`='".$_POST['startdate']."'";
+$result = mysql_query($queryText,$db);
 
 while($row = mysql_fetch_assoc($result)) {
+//	echo "isthisworking";
 	$user = $row['username'];
-	echo '<img src="timesheet2.php?startdate='.$_POST["startdate"]."&username=".$user.'">';
+	echo $user;
+	//echo '<img src="timesheet2.php?startdate='.$_POST["startdate"]."&username=".$user.'">';
 }
 
-}
+
 
 
 
